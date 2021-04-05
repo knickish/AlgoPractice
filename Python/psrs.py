bab78672c88fc3273be2b4ee68267da9c75a19ec
kirk <knickish@gmail.com>
2021-04-05 09:05:31 -0700

    WIP for psrs, partitioning done

--- Python/psrs.py
+++ Python/psrs.py
@@ -62,17 +62,61 @@ def psrs(list_arg):
     cores = multiprocessing.cpu_count()
     if not cores:
         cores = 2
-    def get_pivots(list_arg, p, w):
+    def get_pivot_src(list_arg, p, w):
         psrs_quick(list_arg)
+        indices = [1+(w*x) for x in range(p)]
+        pivot_src = [list_arg[x] for x in indices]
+        return pivot_src
+    
+    def select_pivots(pivot_list, p, n):
+        pi = int(p/(n**2))
+        pivot_src = insertion(pivot_list)
+        pivots = [pivot_src[pi+p*x] for x in range(p)]
+        return pivots
+
+    
+
+    
         
 
 
-    def split_list(listLen, p):
+def multi_partition(list_arg_internal, pivot_vals, l, r):
+        l_int = l
+        r_int = r-1
+        l_int_list = []
+        for pivot_index, pivot in enumerate(pivot_vals):
+            l_int = l
+            r_int = r-1
+            while l_int<=r_int:
+                while l_int<=r_int:
+                    if list_arg_internal[l_int]>=pivot:
+                        break
+                    else:
+                        l_int+=1
+                while l_int<=r_int:
+                    if list_arg_internal[r_int]<pivot:
+                        break
+                    else:
+                        r_int-=1
+                if l_int<r_int:                    
+                    swap(list_arg_internal, l_int, r_int)
+                else:
+                    break
+            l_int_list.append(l_int)
+        
+        l_int_list[0] = (l, l_int_list[0])
+        for i in range(1,len(l_int_list)):
+            l_int_list[i] = (l_int_list[i-1][1], l_int_list[i])
+        return l_int_list
         
     
     #setup
-    if len(list_arg)<2:
-        return list_arg
-    last_list_index = len(list_arg)-1
-    w = n/(p**2)
+    # if len(list_arg)<2:
+    #     return list_arg
+    # last_list_index = len(list_arg)-1
+    # w = n/(p**2)
 
+if __name__ == "__main__":
+    sort_list = [0,1,2,9,16,17,24,25,27,28,30,33]
+    pivot_list = [10,22]
+    print( multi_partition(sort_list, pivot_list, 0, len(sort_list)))
